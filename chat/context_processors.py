ade87552ae64416b37056616c818a766749573f1
handy <dawoudma7moud2019@gmail.com>
2021-09-22 00:14:14 +0600

    change context manager

--- chat/context_processors.py
+++ chat/context_processors.py
@@ -1,10 +1,12 @@
-from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from account.models import Friends
 
 
 def friends(request):
-    if request.user.is_anonymous:
-        return {"friends": ""}
-    return {"friends": Friends.objects.filter(user=request.user)[0].friends.all()}
+    freinds_list = Friends.objects.filter(user=request.user)
+
+    if freinds_list:
+        return {"friends": freinds_list[0].friends.all()}
+    else:
+        return {"friends": freinds_list}
